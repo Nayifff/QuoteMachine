d816167628d4f3151a81ac8d3a152dfdea01670d
Nayifff <nsa2134@columbia.edu>
2019-07-02 14:39:40 -0400

    Updates

---
+++ precache-manifest.42bb019750716e4f783a911f6ad22bc3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f6c0755af9774032bfd6f25d9e2d1c6",
+    "url": "/QuoteMachine/index.html"
+  },
+  {
+    "revision": "9bbddce9f4765ad7a592",
+    "url": "/QuoteMachine/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/QuoteMachine/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "9bbddce9f4765ad7a592",
+    "url": "/QuoteMachine/static/js/main.c37343d5.chunk.js"
+  },
+  {
+    "revision": "ab1f7b065459d22f075b",
+    "url": "/QuoteMachine/static/js/runtime~main.573c12d3.js"
+  }
+]);
